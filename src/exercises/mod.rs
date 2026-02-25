e9df32565420dcc8f433cb92cf48809b3b0c6cbd
Diego Romero <draketromero@gmail.com>
2026-02-24 23:57:55 -0600

    Add: Function to test the solution of "intersection of two arrays"

---
+++ src/exercises/mod.rs
@@ -0,0 +1,9 @@
+pub mod chapter_8;
+
+pub fn test_exercise_1_ch8() {
+    let arr = vec![1, 2, 3, 4, 5, 6];
+    let arr2 = vec![0, 2, 5, 7];
+
+    println!("Los valores del primer array son: {:?} \nY los del segundo son: {:?}\n", arr, arr2);
+    println!("Los elementos que cumplen la interseccion son: {:?}", chapter_8::intersection(&arr, &arr2))
+}
